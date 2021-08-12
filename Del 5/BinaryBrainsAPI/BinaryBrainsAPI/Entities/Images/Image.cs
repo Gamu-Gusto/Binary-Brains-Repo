18f1bf931a1f6a735313773fed1ef76591441835
Poeticluvchild <poeticluvchild@gmail.com>
2021-08-12 03:51:51 +0200

    changing image data type to byte

--- Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Entities/Images/Image.cs
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Entities/Images/Image.cs
@@ -10,7 +10,7 @@ namespace BinaryBrainsAPI.Entities.Images
     {
         [Key]
         public int ImageID { get; set; }
-        public string Image { get; set; }
+        public byte Image { get; set; }
 
         [ForeignKey("ImageTypeID")]
 
