4b6f27fa320aea9e9e311be2ca83fd8c4607d3d5
Poeticluvchild <poeticluvchild@gmail.com>
2021-08-12 03:44:55 +0200

    images Entity

---
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Entities/Images/Image.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinaryBrainsAPI.Entities.Images
+{
+    public class Image
+    {
+        [Key]
+        public int ImageID { get; set; }
+        public string Image { get; set; }
+
+        [ForeignKey("ImageTypeID")]
+
+        public int ImageTypeID { get; set; }
+        public ImageType ImageType { get; set; }
+    }
+}
