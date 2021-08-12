55838373e76fde0aa256e78f7d0efca02cbf3c28
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-12 03:48:27 +0200

    ImageContent attribute

--- Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Entities/Images/Image.cs
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Entities/Images/Image.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace BinaryBrainsAPI.Entities.Images
     {
         [Key]
         public int ImageID { get; set; }
-        public string Image { get; set; }
+        public string ImageContent { get; set; }
 
         [ForeignKey("ImageTypeID")]
 
