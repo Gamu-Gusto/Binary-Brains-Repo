2918228bd172220ad5e9c2f6b3fd5b0cb438fdc8
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-17 14:34:23 +0200

    DbContext bug fixed

--- Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Data/ArtechDbContext.cs
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Data/ArtechDbContext.cs
@@ -11,7 +11,6 @@ using BinaryBrainsAPI.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
