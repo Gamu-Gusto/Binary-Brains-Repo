5ddd97802d9d8de913ecc330c5a65ce5cbf8f953
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-18 14:50:45 +0200

    Schedule CRUD

---
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Repository/ExhibitionsRepositories/ScheduleRepository.cs
@@ -0,0 +1,50 @@
+﻿using BinaryBrainsAPI.Data;
+using BinaryBrainsAPI.Entities.Exhibitions;
+using BinaryBrainsAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinaryBrainsAPI.Repository.ExhibitionsRepositories
+{
+    public class ScheduleRepository : IAppRepository<Schedule>
+    {
+        readonly ArtechDbContext _artechDb;
+
+
+        public ScheduleRepository(ArtechDbContext artechDb)
+        {
+            _artechDb = artechDb;
+        }
+
+        public void Add(Schedule schedule)
+        {
+            _artechDb.Schedule.Add(schedule);
+            _artechDb.SaveChanges();
+        }
+
+        public void Delete(Schedule schedule)
+        {
+            _artechDb.Schedule.Remove(schedule);
+            _artechDb.SaveChanges();
+        }
+
+        public Schedule Get(long id)
+        {
+            return _artechDb.Schedule.FirstOrDefault(s => s.ScheduleID == id);
+        }
+
+        public IEnumerable<Schedule> GetAll()
+        {
+            return _artechDb.Schedule.ToList();
+        }
+
+        public void Update(Schedule schedule, Schedule entity)
+        {
+            schedule.ScheduleName = entity.ScheduleName;
+            schedule.ScheduleDescription = entity.ScheduleDescription;
+            _artechDb.SaveChanges();
+        }
+    }
+}
