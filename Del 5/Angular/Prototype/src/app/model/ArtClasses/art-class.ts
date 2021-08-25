a69cc592c0269f7b368b2248ff6c4d311f031a8b
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-25 15:18:09 +0200

    ArtClass Models Added

---
+++ Del 5/Angular/Prototype/src/app/model/ArtClasses/art-class.ts
@@ -0,0 +1,20 @@
+export class ArtClass {
+    ArtClassID: string;
+    ArtClassName: string;
+    ArtClassDescription: string;
+    ArtClassStartDate: string;
+    ArtClassEndDate: string;
+    ClassLimit: string;
+    RefundDayLimit: string;
+    ClassPrice: string;
+
+    ArtClassType: string;
+
+    Venue: string;
+
+    ClassTeacher: string;
+
+    ArtClassAnnouncement: string;
+
+    Organisation: string;
+}
