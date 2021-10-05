b2937ff09922b22f476a2d6726b7f8531072f355
Poeticluvchild <poeticluvchild@gmail.com>
2021-10-05 07:25:45 +0200

    appltag

---
+++ Del 5/BinaryBrainsAPI/BinaryBrainsAPI/Controllers/ExhibitionsControllers/ApplicationTagController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BinaryBrainsAPI.Controllers.ExhibitionsControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationTagController : ControllerBase
+    {
+        // GET: api/<ApplicationTagController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ApplicationTagController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ApplicationTagController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ApplicationTagController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ApplicationTagController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
